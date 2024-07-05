f95116736327daf2c01e767afe4c8f98ea1cb2a6
Kavitha-1803 <lkavitham@hotmail.com>
2024-07-06 00:54:48 +0530

    converted into object oriented

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
-print("Hello world!")
+class HelloWorld:
+    def __init__(self,mes):
+        self.mes=mes
+    def display(self):
+        print(self.mes)
+
+word=HelloWorld("Hello World")
+word.display()
